07f4582c6f794d16a1742d565163941a919d1c16
Galvin Shem <galvinkla@yahoo.com>
2016-05-17 23:36:20 +0100

    added a new manage listings section

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,16 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
 
+  def seller
+   @listings = Listing.where(user: current_user).order("created_at DESC")
+   end
+  
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all .order("created_at DESC")
   end
 
   # GET /listings/1
@@ -27,7 +31,7 @@ class ListingsController < ApplicationController
   # POST /listings.json
   def create
     @listing = Listing.new(listing_params)
-	
+	@listing.user_id = current_user.id
 
     respond_to do |format|
       if @listing.save
